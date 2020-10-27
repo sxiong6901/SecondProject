058a914117c005c18ce8e5da500cbe1bb5803d7c
sxiong6901 <soua.p.xiong@hotmail.com>
2020-10-26 22:04:09 -0400

    minor changes

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-  const express = require('express');
+const express = require('express');
 const path = require('path')
 const app = express();
 const port = 3000;
@@ -15,22 +14,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log('Example app listening on port ${port}!')
-=======
-  const express = require('express');
-const path = require('path')
-const app = express();
-const port = 3000;
-
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use('/', express.static(path.join(__dirname, 'public')))
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
-
-app.listen(port, () => {
-  console.log('Example app listening on port ${port}!')
->>>>>>> be320fc072dc317e4b4b5dc3c0192eb3a360880e
 });
