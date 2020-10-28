e100e0eeed689bbe983b80f4d2c13dab77670615
Brianca Foster <missbee51@yahoo.com>
2020-10-28 18:55:47 -0400

    SecondProject

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
   const express = require('express');
 const path = require('path')
 const app = express();
@@ -15,22 +14,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log('Example app listening on port ${port}!')
-=======
-  const express = require('express');
-const path = require('path')
-const app = express();
-const port = 3000;
-
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use('/', express.static(path.join(__dirname, 'public')))
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
-
-app.listen(port, () => {
-  console.log('Example app listening on port ${port}!')
->>>>>>> be320fc072dc317e4b4b5dc3c0192eb3a360880e
 });
