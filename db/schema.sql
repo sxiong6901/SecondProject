cf7427f05b253232e3c8ba8c1b5fcd72e04d3d9d
sxiong6901 <soua.p.xiong@hotmail.com>
2020-10-29 22:52:34 -0400

    Update

--- db/schema.sql
+++ db/schema.sql
@@ -5,10 +5,10 @@ CREATE database pizza_db;
 USE pizza_db;
 
 CREATE TABLE pizza (
-  name INTEGER NOT NULL auto_increment PRIMARY KEY,
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
   sauce VARCHAR(30),
   cheese VARCHAR(30),
   meat VARCHAR(30),
   veggies VARCHAR(30),
-  devoured BOOLEAN DEFAULT false
+  PRIMARY KEY (id)
   );
