c55c76fc2196e9c7196542828aa65009add1f218
sxiong6901 <soua.p.xiong@hotmail.com>
2020-11-09 19:18:35 -0500

    update

--- controllers/pizzas_controller.js
+++ controllers/pizzas_controller.js
@@ -2,10 +2,8 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (pizza.js) to use its database functions.
 var pizza = require("../models/pizza.js");
 
-// Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   pizza.all(function(data) {
     var hbsObject = {
@@ -21,7 +19,7 @@ router.post("/api/pizzas", function(req, res) {
   ], [
     req.body.name, req.body.devoured
   ], function(result) {
-    // Send back the ID of the new pizza
+
     res.json({ id: result.insertId });
   });
 });
@@ -32,7 +30,7 @@ router.put("/api/pizzas/:id", function(req, res) {
    devoured: 1
   }, condition, function(result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
+
       return res.status(404).end();
     } else {
       res.status(200).end();
